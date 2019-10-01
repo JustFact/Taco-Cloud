09dfc8738ccde34fd0e8fd3b09d5b5e843ab7d17
JustFact <jaspreetsingh6881@gmail.com>
2019-10-01 20:51:53 +0530

    Added Order Controller

---
+++ src/main/java/tacos/OrderController.java
@@ -0,0 +1,20 @@
+package tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+	
+	@GetMapping("/current")
+	public String orderForm(Model model) {
+		model.addAttribute("oder", new Order());
+		return "orderForm";
+	}
+}
