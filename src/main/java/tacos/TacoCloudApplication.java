d31f206600897fab4dd0376061dcefee90e76588
JustFact <jaspreetsingh6881@gmail.com>
2019-10-04 20:20:52 +0530

    Added addViewController() for homepage '/'

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -2,6 +2,7 @@ package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -9,6 +10,10 @@ public class TacoCloudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
 		//Hello world
+	}
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
 	}
 
 }
