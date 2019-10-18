d259236f548fa87fd12862cd24fff6fbf0300e0a
JustFact <jaspreetsingh6881@gmail.com>
2019-10-18 16:05:32 +0530

    saving Order in the database

--- src/main/java/tacos/data/JdbcOrderRepository.java
+++ src/main/java/tacos/data/JdbcOrderRepository.java
@@ -1,5 +1,11 @@
 package tacos.data;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
@@ -7,6 +13,7 @@ import org.springframework.stereotype.Repository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import tacos.Order;
+import tacos.Taco;
 
 @Repository
 public class JdbcOrderRepository implements OrderRepository{
@@ -15,6 +22,8 @@ public class JdbcOrderRepository implements OrderRepository{
 	private SimpleJdbcInsert orderTacoInserter;
 	private ObjectMapper objectMapper;
 	
+	
+	@Autowired
 	public JdbcOrderRepository(JdbcTemplate jdbc) {
 		this.orderInserter = new SimpleJdbcInsert(jdbc)
 				.withTableName("Taco_Order")
@@ -28,8 +37,43 @@ public class JdbcOrderRepository implements OrderRepository{
 	
 	@Override
 	public Order save(Order order) {
-		// TODO Auto-generated method stub
-		return null;
+		order.setPlacedAt(new Date());
+		long orderId = saveOrderDetails(order);
+		order.setId(orderId);
+		List<Taco> tacos = order.getTacos();
+		
+		for(Taco taco : tacos) {
+			saveTacoToOrder(taco, orderId);
+		}
+		
+		return order;
+	}
+	
+	private long saveOrderDetails(Order order) {
+		Map<String, Object> values = objectMapper.convertValue(order, Map.class);
+		values.put("placedAt", order.getPlacedAt());
+		
+		long orderId = orderInserter.executeAndReturnKey(values).longValue();
+		return orderId;
+	}
+	
+	private void saveTacoToOrder(Taco taco, long orderId) {
+		Map<String, Object> values = new HashMap<>();
+		values.put("tacoOrder", orderId);
+		values.put("taco", taco.getId());
+		orderTacoInserter.execute(values);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
