be090ef97669ce8ab02b015f78fc1d9f8524a35b
JustFact <jaspreetsingh6881@gmail.com>
2019-10-22 17:09:55 +0530

    created JPA User Repository with Registration form

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,12 +1,14 @@
-package security;
+package tacos.security;
 
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
@@ -14,18 +16,17 @@ import org.springframework.security.crypto.password.StandardPasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
-	DataSource dataSource;
+	private UserDetailsService userDetailsService;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication()
-		.dataSource(dataSource)
-		.usersByUsernameQuery("select username, password, enabled from Users "
-				+ "where username=?")
-		.authoritiesByUsernameQuery("select username, authority from UserAuthorities "
-				+ "where username=?")
-		.passwordEncoder(new StandardPasswordEncoder("53cr3t"));
-		
+		auth.userDetailsService(userDetailsService)
+		.passwordEncoder(encoder());
+	}
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return new StandardPasswordEncoder("53cr3t");
 	}
 	
 }
